5632cca077649013d33d42c429f10197a8274475
adr400bur@gmail.com <adr400bur@gmail.com>
2023-02-04 12:29:16 -0800

    say Hello HW

---
+++ HW sayHello.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	
+    cout << "Say Hello" << endl;
+
+    string userInput;
+
+
+	cout << "Please enter your name?" << endl;
+    cin >> userInput;
+
+    for (int i=0; i<=6; i++)
+    {
+        cout << endl;
+    }
+
+    cout << "Hello " << userInput << " how are you today>?" << endl;
+    cin >> userInput;
+
+
+    cout << "Have a nice day!" << endl;
+
+
+	return 0;
+}
